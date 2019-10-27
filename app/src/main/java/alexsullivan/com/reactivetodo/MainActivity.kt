b399f454192e3cb62d95076980723a58fc9e2bec
Alex Sullivan <Alexsullivan114@gmail.com>
2019-10-27 11:08:57 -0400

    Move callback out of main activity

--- app/src/main/java/alexsullivan/com/reactivetodo/MainActivity.kt
+++ app/src/main/java/alexsullivan/com/reactivetodo/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.fab
 import kotlinx.android.synthetic.main.activity_main.list
 import kotlinx.coroutines.flow.collect
@@ -22,19 +21,9 @@ class MainActivity : AppCompatActivity() {
     val adapter = TodoAdapter { viewModel.todoUpdated(it) }
 
 
-    val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
-      override fun onMove(
-        recyclerView: RecyclerView,
-        viewHolder: RecyclerView.ViewHolder,
-        target: RecyclerView.ViewHolder
-      ): Boolean {
-        return false
-      }
-
-      override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        val item = adapter.items[viewHolder.adapterPosition]
-        viewModel.todoDeleted(item)
-      }
+    val callback = DismissHelper {
+      val item = adapter.items[it.adapterPosition]
+      viewModel.todoDeleted(item)
     }
 
     ItemTouchHelper(callback).attachToRecyclerView(list)
